3aa1aa858fac0e9a506a197cf136585e5f604eb9
juyonglee <juyonglee0208@gmail.com>
2020-05-11 13:24:47 +0900

    의존성 주입 설정

--- 02. 스프링 특징과 의존성 주입/Example02_XML/src/main/java/com/gmail/juyonglee0208/Restaurant.java
+++ 02. 스프링 특징과 의존성 주입/Example02_XML/src/main/java/com/gmail/juyonglee0208/Restaurant.java
@@ -1,9 +1,13 @@
 package com.gmail.juyonglee0208;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import lombok.Data;
 import lombok.Setter;
 
+@Data
+@Component
 public class Restaurant {
 	@Setter(onMethod_ = @Autowired)
 	private Chef chef;
