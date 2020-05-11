f6aeb06ed9cd245b9dc64131ccee19f15e451606
juyonglee <juyonglee0208@gmail.com>
2020-05-11 15:27:03 +0900

    Componentscan 추가

--- 02. 스프링 특징과 의존성 주입/Example02_JAVA/src/main/java/com/gmail/juyonglee0208/RootConfig.java
+++ 02. 스프링 특징과 의존성 주입/Example02_JAVA/src/main/java/com/gmail/juyonglee0208/RootConfig.java
@@ -1,8 +1,10 @@
 package com.gmail.juyonglee0208;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = {"com.gmail.juyonglee0208"})
 public class RootConfig {
 
 }
