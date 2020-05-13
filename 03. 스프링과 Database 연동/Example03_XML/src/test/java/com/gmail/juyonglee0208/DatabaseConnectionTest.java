0b499d0ad79dd1ef33cb12176b6630872efe585d
juyonglee <juyonglee0208@gmail.com>
2020-05-13 10:46:15 +0900

    Connection Pool Test 추가

--- 03. 스프링과 Database 연동/Example03_XML/src/test/java/com/gmail/juyonglee0208/DatabaseConnectionTest.java
+++ 03. 스프링과 Database 연동/Example03_XML/src/test/java/com/gmail/juyonglee0208/DatabaseConnectionTest.java
@@ -6,17 +6,24 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class DatabaseConnectionTest {
+	
+	@Setter(onMethod_ = @Autowired)
+	private DataSource datasource;
 	
 	@Test
 	public void connectionTest() {
@@ -29,5 +36,18 @@ public class DatabaseConnectionTest {
 			fail();
 			log.error(e.getMessage());
 		}
+	}
+	
+	@Test
+	public void connectionPoolTest() {
+		try {
+			Connection connection = datasource.getConnection();
+			log.info("-----------------------");
+			log.info("[Connection Pool Test]");
+			log.info(connection);
+			log.info("-----------------------");
+		} catch (SQLException e) {
+			fail(e.getMessage());
+		}	
 	}
 }
